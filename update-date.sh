5cfe2b81ce22223cb7d8138cb56d45d3922f19dc
yeonnex <yeonnex@gmail.com>
2025-01-01 17:39:23 +0900

    docs: 쿠버네티스 이해하기

--- update-date.sh
+++ update-date.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # 파일 내 'Update Date:' 부분을 현재 날짜로 변경하는 스크립트
+# Intellij 의 File Watcher 플러그인이 cmd + s 키보드 트리거를 받으면 본 스크립트 실행
 
-# 현재 날짜를 'yy. MM. dd.' 형식으로 얻기
-CURRENT_DATE=$(date "+%y. %m. %d.")
+# 현재 날짜를 'yyyy-MM-dd' 형식으로 얻기
+CURRENT_DATE=$(date "+%Y-%m-%d")
 
 # Update Date 부분을 찾아서 현재 날짜로 갱신
-sed -i '' "s/<!-- Update Date: [^}]* } -->/<!-- Update Date: $CURRENT_DATE } -->/" "$1"
+sed -i '' "s/<!-- Update Date: [^}]* -->/<!-- Update Date: $CURRENT_DATE -->/" "$1"
