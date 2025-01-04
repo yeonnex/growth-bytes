aa68989f2198f164438e0ab41243c4f83978f063
yeonnex <yeonnex@gmail.com>
2025-01-04 10:57:05 +0900

    docs: 애플리케이션 빌드 실전 예제(java, node, go)

--- update-date.sh
+++ update-date.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # 파일 내 'Update Date:' 부분을 현재 날짜로 변경하는 스크립트
-# Intellij 의 File Watcher 플러그인이 cmd + s 키보드 트리거를 받으면 본 스크립트 실행
 
 # 현재 날짜를 'yyyy-MM-dd' 형식으로 얻기
 CURRENT_DATE=$(date "+%Y-%m-%d")
 
-# Update Date 부분을 찾아서 현재 날짜로 갱신
-sed -i '' "s/<!-- Update Date: [^}]* -->/<!-- Update Date: $CURRENT_DATE -->/" "$1"
+# 파일에서 현재 Update Date 확인 (확장 정규식 사용)
+FILE_DATE=$(grep -oE '<!-- Update Date: [^<]* -->' "$1" | sed -E 's/<!-- Update Date: (.*) -->/\1/')
+
+# 디버깅 출력
+echo "현재 날짜: $CURRENT_DATE"
+echo "파일 날짜: $FILE_DATE"
+
+# 현재 날짜와 다를 경우에만 파일 수정
+if [ "$CURRENT_DATE" != "$FILE_DATE" ]; then
+  echo "날짜가 다릅니다. 파일을 수정합니다."
+  sed -i '' "s/<!-- Update Date: [^<]* -->/<!-- Update Date: $CURRENT_DATE -->/" "$1"
+else
+  echo "파일이 최신 상태입니다."
+fi
